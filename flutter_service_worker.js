2186b46b3cda7c7a9d0db62014e30d698f1a7dbf
shukebeta <shukebeta@users.noreply.github.com>
2025-05-17 09:22:53 +0000

    deploy: f7c9ea492bab20925b4ea03b6cdaf4851ae77b50

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -30,7 +30,7 @@ const RESOURCES = {"icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2e
 "canvaskit/chromium/canvaskit.js": "34beda9f39eb7d992d46125ca868dc61",
 "canvaskit/chromium/canvaskit.wasm": "64a386c87532ae52ae041d18a32a3635",
 "canvaskit/chromium/canvaskit.js.symbols": "5a23598a2a8efd18ec3b60de5d28af8f",
-"flutter_bootstrap.js": "8cce9832938c9c413e4e1e2f651e8d77",
+"flutter_bootstrap.js": "7ffd3e1a5ef85bba00521f9c860591bf",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "index.html": "222fe90efceb30751644a7d2147a1ab2",
 "/": "222fe90efceb30751644a7d2147a1ab2",
